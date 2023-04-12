3296def079cfe6246df66673d8f278e8706d7094
Victor Hugo B. S. Miranda <victor-f250@hotmail.com>
2023-04-12 15:31:19 -0300

    inicio

---
+++ src/main/java/com/vic/profissionalinfo/ProfissionalinfoApplication.java
@@ -0,0 +1,13 @@
+package com.vic.profissionalinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfissionalinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfissionalinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vic/profissionalinfo/ProfissionalinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vic.profissionalinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfissionalinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
